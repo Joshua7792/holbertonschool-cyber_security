0d613aec64a6d8c5048b4c6c7411f7700e69e221
Joshua7792 <j.santiago39@hotmail.com>
2024-12-10 15:56:38 -0500

    A record fixed lines

--- network_security/0x01_passive_reconnaissance/1-a_record.sh
+++ network_security/0x01_passive_reconnaissance/1-a_record.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-# Check if domain argument is not empty 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <domain>"
-    exit 1
-fi
-
-# nslookup to retrieve only the A record of the domain
 nslookup -query=A $1
+# # Check if domain argument is not empty 
+# if [ -z "$1" ]; then
+#     echo "Usage: $0 <domain>"
+#     exit 1
+# fi
