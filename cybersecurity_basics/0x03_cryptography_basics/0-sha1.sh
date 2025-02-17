a4abb3b1d9536b27530c679e86d492d31e5d012a
joshua7792 <j.santiago.39@hotmail.com>
2025-02-17 00:21:30 -0400

    2 lines

--- cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
+++ cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Check if an argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <password>"
-    exit 1
-fi
-
-# Generate SHA-1 hash of the input password and store it in 0_hash.txt
-echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt
+[ -z "$1" ] && echo "Usage: $0 <password>" && exit 1 || echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt
