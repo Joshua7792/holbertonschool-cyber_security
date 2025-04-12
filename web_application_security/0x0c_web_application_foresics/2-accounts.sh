efa6d1a8a1cd2bdeb533165ff1ea46963c10591b
joshua7792 <j.santiago.39@hotmail.com>
2025-04-11 22:57:32 -0400

    fixed number 2

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log | awk '/Failed password/ {f[$NF]++} /Accepted password/ {s[$NF]++} END {for (u in f) if (s[u]) print u}'
+tail -n 1000 auth.log | awk '/Failed password/ {f[$9]++} /Accepted password/ {s[$9]++} END {for (u in s) if (f[u] && s[u]) print u}'
