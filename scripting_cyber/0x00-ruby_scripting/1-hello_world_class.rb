b31051df34a3897a27b4fb03ec3403f75cd2b683
joshua7792 <j.santiago.39@hotmail.com>
2025-05-08 17:59:54 -0400

    class instance variable

---
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# Class HelloWorld with variable
+
+class HelloWorld
+    def initialize
+        @message = 'Hello, World!'
+    end
+
+    def print_hello
+        puts @message
+    end
+end
